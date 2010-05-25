fdf66c789f2b8ca5b9d2d698af2bac59e849bada
Ryan Buterbaugh <ryan@buterbaugh.org>
2010-05-25 22:06:56 +0000

    fix formatting of fieldselection

--- jquery-fieldselection-0.1.0.js
+++ jquery-fieldselection-0.1.0.js
@@ -1,6 +1,7 @@
 /*
  * jQuery plugin: fieldSelection - v0.1.0 - last change: 2006-12-16
  * (c) 2006 Alex Brem <alex@0xab.cd> - http://blog.0xab.cd
+ * 
  * fieldSelection - a jQuery plugin
  * 
  * Copyright (c) 2006, Alex Brem
